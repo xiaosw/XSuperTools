4e44290b1f288eb7156cf9b3157fc14dd1e4365a
xiaosw <xiaosw@putao.com>
2017-03-07 17:05:31 +0800

    添加kotlin支持

---
+++ app/src/main/java/com/xiaosw/tool/activity/KotlinActivity.kt
@@ -0,0 +1,13 @@
+package com.xiaosw.tool.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.xiaosw.tool.R
+
+class KotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
